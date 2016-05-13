a1d1d6026a216d7033c18bb6355955a1500d5f31
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-05-13 11:38:01 -0400

    Extracted OutputStringToConsole from the GetUserName method

--- COMP-123-S2016-Lesson1B/Program.cs
+++ COMP-123-S2016-Lesson1B/Program.cs
@@ -11,9 +11,9 @@ using System.Threading.Tasks;
  * 
  * Description: This program demonstrate GitHub and version control best practice. We also explore Unit Testing
  * 
- * Version: 0.3 - Updated comments for driver class Prodram
+ * Version: 0.4 - Extracted OutputStringToConsole from the GetUserName method
  * 
- */ 
+ */
 
 namespace COMP_123_S2016_Lesson1B
 {
@@ -49,13 +49,36 @@ namespace COMP_123_S2016_Lesson1B
             //Initial Variables
             string UserName = "";
 
-            Console.Write("Enter User Name: ");
+            OutputStringToConsole("Enter the User Name: ", false);
             UserName = Console.ReadLine();
 
-            Console.WriteLine("=====================");
-            Console.WriteLine("You Entered: {0}", UserName);
+            Console.WriteLine("\n=====================\n");
+            Console.WriteLine("You Entered: {0}", UserName + "\n");
 
             return UserName;
+        }
+
+        /**
+         * This method writes a string to the console. Paramerers allow the user to choose to add a newLine character
+         * 
+         * @method OutputStringToConsole
+         * @param {string} outputString 
+         * @param {bool} has NewLine
+         * @return {string} outputString 
+         */
+
+        private static string OutputStringToConsole(string outputString, bool hasNewLine)
+        {
+            if(hasNewLine)
+            {
+                Console.WriteLine(outputString);
+            }
+            else
+            {
+                Console.Write(outputString);
+            }
+
+            return outputString;
         }
     }
 }
