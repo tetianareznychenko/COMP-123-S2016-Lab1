00afb3c9cd1d25ffc8383269d6dc3eb90917e62a
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-05-13 10:44:04 -0400

    Initial Commit

--- COMP-123-S2016-Lesson1B/Program.cs
+++ COMP-123-S2016-Lesson1B/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author: Tetiana Reznychenko
+ * Date: May 13, 2016
+ * ID: 300870231
+ * 
+ * Description: This program demonstrate GitHub and version control best practice. We also explore Unit Testing
+ * 
+ * Version: 0.1 - Initial Version
+ */ 
+
 namespace COMP_123_S2016_Lesson1B
 {
     class Program
